0ed8cd3b9347bdbc64bd3d3703ff7a5b945dd84b
ValcirJr <balbinottijr@outlook.com>
2022-05-12 20:07:25 -0300

    Initial commit

---
+++ src/br/edu/utfpr/Main.java
@@ -0,0 +1,19 @@
+package br.edu.utfpr;
+/*
+Criar programa orientado à objetos para solucionar os seguintes casos de uso:
+- Será o controle de uma biblioteca
+- Desenvolver método para a locação de livro
+- deverá validar se livro está disponível
+- Método para reserva de livro
+- Validá se há alguma reserva para data e se estará disponível
+- Método para devolução do livro
+- Válida se a data da entrega é anterior a data máxima
+- caso tenha vencido calcular 50 centavos ao dia até 20 reais, depois 1 real por dia
+
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
